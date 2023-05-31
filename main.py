3454f911c2da3046e0c5b68c4cf11deff0817c8f
Dom Jina <dominicjina@gmail.com>
2023-05-31 14:48:48 +0100

    implemented PCA

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from matplotlib import pyplot as plt
 import scipy
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.decomposition import PCA
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import numpy as np
 import os
@@ -61,23 +62,27 @@ def neural_network() -> None:
 
 
 def k_means_clustering(k: int) -> tuple[float, float, float, float]:
-    KNN_train_X = np.array(train_X).reshape(60000, 784)
+    pca = PCA(n_components=50)
+    KMN_train_X = np.array(train_X).reshape(60000, 784)
+    pca.fit(KMN_train_X)
+    KMN_train_X = pca.transform(KMN_train_X)
+    # print(KMN_train_X.shape)
     NUM_OF_CENTROIDS = k
-    centroids = KNN_train_X[np.random.choice(KNN_train_X.shape[0], NUM_OF_CENTROIDS, replace = False)]
+    centroids = KMN_train_X[np.random.choice(KMN_train_X.shape[0], NUM_OF_CENTROIDS, replace = False)]
     delta = 10000
     counter = 0
     while np.all(delta > 1e-7):
         counter += 1
-        distances = KNN_train_X.dot(centroids.T)
+        distances = KMN_train_X.dot(centroids.T)
         labels = np.argmax(distances, axis=1)
-        new_centroids = np.zeros((NUM_OF_CENTROIDS, KNN_train_X.shape[1]))
+        new_centroids = np.zeros((NUM_OF_CENTROIDS, KMN_train_X.shape[1]))
 
         for i in range(NUM_OF_CENTROIDS):
             indices = np.where(labels == i)[0]
             if len(indices) > 0:
-                mean_vec = np.sum(KNN_train_X[indices], axis=0) / len(indices)
+                mean_vec = np.sum(KMN_train_X[indices], axis=0) / len(indices)
             else:
-                mean_vec = np.zeros(KNN_train_X.shape[1])
+                mean_vec = np.zeros(KMN_train_X.shape[1])
             new_centroids[i] = mean_vec
 
         delta = np.linalg.norm(new_centroids - centroids, axis=1)
@@ -86,7 +91,7 @@ def k_means_clustering(k: int) -> tuple[float, float, float, float]:
     label_names = [i for i in range(0,10)]
     labels_in_centroids = [[0]*NUM_OF_CENTROIDS for _ in range(len(label_names))]
 
-    distances = KNN_train_X.dot(centroids.T)
+    distances = KMN_train_X.dot(centroids.T)
     labels = np.argmax(distances, axis=1)
 
     for i, centroid in enumerate(centroids):
@@ -103,6 +108,8 @@ def k_means_clustering(k: int) -> tuple[float, float, float, float]:
     # print(label_counts)
 
     KMN_test_X = np.array(test_X).reshape(10000, 784)
+    KMN_test_X = pca.transform(KMN_test_X)
+    # print(KMN_test_X.shape)
     distances = KMN_test_X.dot(centroids.T)
     labels = np.argmax(distances, axis=1)
     y_pred = np.zeros(KMN_test_X.shape[0], dtype=int)
@@ -125,6 +132,7 @@ if __name__ == "__main__":
     test_X = tf.keras.utils.normalize(test_X, axis=1)
 
     # neural_network()
+    
     best_k = 0
     best_accuracy = 0.0
     best_precision = 0.0
